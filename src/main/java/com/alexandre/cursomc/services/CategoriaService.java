9528c5c328a018fd40d46fa10d0da9a9d41be9ee
Alexandre-Azvo <azvo.ads@gmail.com>
2020-01-26 09:45:43 -0300

    Revert "Ajustes no endponit /categorias/{id}"
    This reverts commit 41064f5476eb8b7713f6488af4b4b5a27fa8a133.

--- src/main/java/com/alexandre/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexandre/cursomc/services/CategoriaService.java
@@ -7,18 +7,15 @@ import org.springframework.stereotype.Service;
 
 import com.alexandre.cursomc.domain.Categoria;
 import com.alexandre.cursomc.repositories.CategoriaRepository;
-import com.alexandre.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-
+	
 	@Autowired
 	private CategoriaRepository repo;
-
+	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		return obj.orElse(null);
 	}
 }
